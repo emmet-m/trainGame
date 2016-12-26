598edac5089fae864a7f4c32c361128240b15797
root <e.minor.murray@gmail.com>
2016-12-26 11:18:49 +0000

    implemented error function and further checking

--- trainGame.c
+++ trainGame.c
@@ -1,18 +1,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+static void exitWithUsage(int type);
+
 int main(int argc, char * argv[]) {
+
+	// Check that the user has entered 4 arguments
+	if (argc != 5) exitWithUsage(0);
+
 	// extract our 4 arguments
 	int a, b, c, d;
-	if(! sscanf(argv[1], "%d", &a)) exit(0);
-	if(! sscanf(argv[2], "%d", &b)) exit(0);
-	if(! sscanf(argv[3], "%d", &c)) exit(0);
-	if(! sscanf(argv[4], "%d", &d)) exit(0);
-	printf("%d %d %d %d\n", a, b, c, d);
+
+	// If failed to scan for a number, exit and print program usage
+	if(! sscanf(argv[1], "%d", &a)) exitWithUsage(1);
+	if(! sscanf(argv[2], "%d", &b)) exitWithUsage(1);
+	if(! sscanf(argv[3], "%d", &c)) exitWithUsage(1);
+	if(! sscanf(argv[4], "%d", &d)) exitWithUsage(1);
+
+	// Display the user input
+	printf("Numbers: %d %d %d %d\n", a, b, c, d);
+
 	return EXIT_SUCCESS;
 }
 
-static void exitWithUsage(void) {
+static void exitWithUsage(int type) {
 	printf("Usage: (0-9) (0-9) (0-9) (0-9)\n");
+	switch (type) {
+		case(0):
+			printf("Error: program requires exactly 4 arguments\n");
+			break;
+		case(1):
+			printf("Error: Arguments must be integer numbers\n");
+			break;
+	}	
 	exit(0);
 }
