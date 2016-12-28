46759d326d7ee9c1893071c8fd953a5a077d0a4e
root <e.minor.murray@gmail.com>
2016-12-28 04:29:58 +0000

    Changed formatting for BST

--- BST.h
+++ BST.h
@@ -2,19 +2,19 @@
 
 typedef Node * Link;
 typedef struct Node {
-	// A number for use in the game
-	int data;
-	
-	// A type of struct.
-	// 'o' for operand
-	// 'n' for number
-	char type;
+    // A number for use in the game
+    int data;
 
-	// A character defining the operand on the node.
-	// Either *, /, + or -.
-	char op;
+    // A type of struct.
+    // 'o' for operand
+    // 'n' for number
+    char type;
 
-	// Connections to other nodes
-	Link right;
-	Link left;	
+    // A character defining the operand on the node.
+    // Either *, /, + or -.
+    char op;
+
+    // Connections to other nodes
+    Link right;
+    Link left;	
 } Node;
