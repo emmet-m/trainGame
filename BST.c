84747ed37767fb83c664c394466bce989ac1e818
root <e.minor.murray@gmail.com>
2016-12-28 05:06:12 +0000

    Finalised node creation function

--- BST.c
+++ BST.c
@@ -2,4 +2,19 @@
 #include <stdlib.h>
 #include "BST.h"
 
+Link createNode(char type, int data, char op) {
+    // Malloc a new node. If it's null, return NULL.
+    Link new = malloc(sizeof(Node));
+    if (new == NULL) return NULL;
 
+    // Set the node's new values
+    new->type = type;
+    new->data = data;
+    new->op = op;
+
+    new->left = NULL;
+    new->right = NULL;
+
+    // Give it back to the user
+    return new;
+}
--- BST.h
+++ BST.h
@@ -1,14 +1,16 @@
 // BST: an ADT for creating and using binary search trees
 
-typedef Node * Link;
-typedef struct Node {
-    // A number for use in the game
-    int data;
+// Connections between nodes on tree's are links
+typedef struct Node * Link;
 
+typedef struct Node {
     // A type of struct.
     // 'o' for operand
     // 'n' for number
     char type;
+
+    // A number for use in the game
+    int data;
 
     // A character defining the operand on the node.
     // Either *, /, + or -.
@@ -17,4 +19,7 @@ typedef struct Node {
     // Connections to other nodes
     Link right;
     Link left;	
+
 } Node;
+
+Link createNode(char type, int data, char op);
