a44bae8fee91c6294fca30ca0e14bb5d715e1d7e
root <e.minor.murray@gmail.com>
2016-12-28 05:13:15 +0000

    Moved around definitions

--- BST.c
+++ BST.c
@@ -2,10 +2,31 @@
 #include <stdlib.h>
 #include "BST.h"
 
+// The definition of a tree node
+typedef struct Node {
+    // A type of struct.
+    // 'o' for operand
+    // 'n' for number
+    char type;
+
+    // A number for use in the game
+    int data;
+
+    // A character defining the operand on the node.
+    // Either *, /, + or -.
+    char op;
+
+    // Connections to other nodes
+    Link right;
+    Link left;	
+
+} Node;
+
+
 Link createNode(char type, int data, char op) {
     // Malloc a new node. If it's null, return NULL.
     Link new = malloc(sizeof(Node));
-    if (new == NULL) return NULL;
+    assert(new != NULL);
 
     // Set the node's new values
     new->type = type;
--- BST.h
+++ BST.h
@@ -3,23 +3,6 @@
 // Connections between nodes on tree's are links
 typedef struct Node * Link;
 
-typedef struct Node {
-    // A type of struct.
-    // 'o' for operand
-    // 'n' for number
-    char type;
-
-    // A number for use in the game
-    int data;
-
-    // A character defining the operand on the node.
-    // Either *, /, + or -.
-    char op;
-
-    // Connections to other nodes
-    Link right;
-    Link left;	
-
-} Node;
-
+// A function for creating a node. All items are set,
+// but data or op will be ignored by other functions depending on type.
 Link createNode(char type, int data, char op);
