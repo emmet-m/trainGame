f6260c7c6155a84536fe904af91da0d78ff92435
Emmet Murray <e.minor.murray@gmail.com>
2017-05-11 14:37:41 +1000

    tree definition

--- BST.h
+++ BST.h
@@ -3,6 +3,10 @@
 // Connections between nodes on tree's are links
 typedef struct Node * Link;
 
+// Tree is just a pointer to a node!
+typedef Node * Tree;
+
+
 // A function for creating a node. All items are set,
 // but data or op will be ignored by other functions depending on type.
 Link createNode(char type, int data, char op);
